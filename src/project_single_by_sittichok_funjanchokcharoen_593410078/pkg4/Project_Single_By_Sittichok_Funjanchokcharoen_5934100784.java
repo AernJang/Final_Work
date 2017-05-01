9ad0897b7d1567d594c55d09ab55cadfc359a41d
DESKTOP-VTBLHFR\USER <aernjang1997@gmail.com>
2017-05-01 23:34:28 +0700

    Pj

---
+++ src/project_single_by_sittichok_funjanchokcharoen_593410078/pkg4/Project_Single_By_Sittichok_Funjanchokcharoen_5934100784.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_single_by_sittichok_funjanchokcharoen_593410078.pkg4;
+
+/**
+ *
+ * @author USER
+ */
+public class Project_Single_By_Sittichok_Funjanchokcharoen_5934100784 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        memu run = new memu();
+        run.setVisible(true);
+    }
+    
+}
